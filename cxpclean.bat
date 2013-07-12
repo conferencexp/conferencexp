d2a7fe30530be96ac4ab474126fe7fc54786e1ec
Fred Videon <fred@cs.washington.edu>
2013-07-12 16:15:05 -0700

    Update to clean script so that it won't interfere with git configuration

--- cxpclean.bat
+++ cxpclean.bat
@@ -1,12 +1,12 @@
 :: Clean output folders
 
-for /D /R %%i in (bin*) do (rd /s /q "%%i")
-for /D /R %%i in (obj*) do (rd /s /q "%%i")
-for /D /R %%i in (redist*) do (rd /s /q "%%i")
-for /D /R %%i in (debug*) do (rd /s /q "%%i")
-for /D /R %%i in (release*) do (rd /s /q "%%i")
-for /D /R %%i in (precompiledweb*) do (rd /s /q "%%i")
-for /D /R %%i in (x64*) do (rd /s /q "%%i")
+for /D /R %%i in (bin?) do (rd /s /q "%%i")
+for /D /R %%i in (obj?) do (rd /s /q "%%i")
+for /D /R %%i in (redist?) do (rd /s /q "%%i")
+for /D /R %%i in (debug?) do (rd /s /q "%%i")
+for /D /R %%i in (release?) do (rd /s /q "%%i")
+for /D /R %%i in (precompiledweb?) do (rd /s /q "%%i")
+for /D /R %%i in (x64?) do (rd /s /q "%%i")
 
 :: Clean generated files
 
